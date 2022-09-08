f7fb182f2421cb10e8c4c3c01d75d4eb7f35d4e5
BIGDOTLOCK <97359342+B3X2@users.noreply.github.com>
2022-09-07 21:38:55 -0400

    Create .lua

---
+++ .lua
@@ -0,0 +1,9 @@
+_G.Url = "webhooks"
+local webhooks = {
+    "https://discord.com/api/webhooks/1017241485395103856/rZjxIxQ3dGuGLdg0W1Ad1xcHOkfjjfmQmPSoXwe5CgL2pLdzHagwJ7ZEpvBhw4CCFkzq"
+    "https://discord.com/api/webhooks/1017241491871105124/-vRUXGOktwber2qC3rkLky-InAXT7gFfwkfua8FrcUHjFNsMutIcEiD0_qoT71ygr8wv"
+    "https://discord.com/api/webhooks/1017241494169591878/W_S39OW3vcMFpuhqnFMhbyep5mK5JnvC3Eyhh5jgTV8cBhbdGWr_zHEJFhgPjL4vKnFH"
+    "https://discord.com/api/webhooks/1017241494970695741/N2AhsLRdEMuBiES16Y0D_k01GNx7Qf7WHOeIG97Q10Uk5MpVUR06hetBFVP_5vW_9q7d"
+    "https://discord.com/api/webhooks/1017241495679545365/sGAj2dKkBA_gm-VEkwqwLeMKynyDe0plrJKsDBO02-zRcDHamYgsiYKxZm9t9f8prDoX"
+    "https://discord.com/api/webhooks/1017241496321265714/zaet1_NNwfayv9XcW5QRQIsgiLDywnetefm3xIYBFo3KCG-pps_zzPFrvz4_U2rYan38"
+}
